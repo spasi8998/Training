08c581bd8b5198e87b459cd4cd5df3f06c3d58aa
spasi8998 <gconte975@gmail.com>
2019-10-08 10:41:47 +0200

    dichiarazione somma

--- esercizio.cpp
+++ esercizio.cpp
@@ -5,5 +5,14 @@ using namespace std;
 int main() {
 	int a,b,c;
 	
+	cout<<"inserisci primo numero"<<a<<endl;
+	cout<<"inserisci secondo numero"<<b<<endl;
+	
+	c=a+b;
+	cout<<"la somma e"<<c<<endl;
+	
+	
+	
+	
 	
 }
